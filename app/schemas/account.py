8efbf404ce4f3ab6978076b373e410eda1e92520
minchaekeem <leehyeon@Leeui-MacBookPro.local>
2025-06-18 19:00:21 +0900

    feat: add a new schema about token for JWT support
    JWT 토큰 관련  스키마 추가

--- app/schemas/account.py
+++ app/schemas/account.py
@@ -22,3 +22,16 @@ class AccountLogin(BaseModel):
 
     email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    """토큰 응답 스키마"""
+
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    """토큰 데이터 스키마"""
+
+    email: Optional[str] = None
