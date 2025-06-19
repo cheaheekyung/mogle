243be15f8caba02e3ee75d4609a5cf09c3ab92b3
minchaekeem <leehyeon@Leeui-MacBookPro.local>
2025-06-19 17:36:50 +0900

    feat: add create_access_token function
    create_access_token 함수 추가

--- app/core/security.py
+++ app/core/security.py
@@ -1,15 +1,18 @@
 import os
+from datetime import datetime, timedelta, timezone
+from typing import Optional
 
 from dotenv import load_dotenv
+from jose import jwt
 from passlib.context import CryptContext
 
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-ALGORITHM = 'HS256'
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -20,3 +23,15 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def get_password_hash(password: str) -> str:
     """비밀번호 해싱"""
     return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    """JWT 액세스 토큰 생성"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
